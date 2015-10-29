da97560e24f4856b65a98f77c8a022a1363dfe3f
GMohanty <gouravmoy.mohanty91@gmail.com>
2015-10-29 21:45:22 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-var express = require('express')
-app = express();
-cons = require('consolidate');
+var express = require('express'),
+    app = express(),
+    cons = require('consolidate'),
+    MongoServer = require('mongodb').Server,
+    Db = require('mongodb').Db;
+
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
-app.set('views', __dirname + "/views");
+app.set('views', __dirname + '/views');
+
+db = new Db('test', new MongoServer('localhost', 27017, {'native_parser': true}));
 
 app.get('/', function (req, res) {
-    //res.send("Hello World")
-    res.render('hello', {'name': 'Gourav'});
+
+    // Find one document in our collection
+    db.collection('people').findOne({}, function (err, doc) {
+        if (err) throw err;
+        console.log(doc);
+        res.render('hello', doc);
+    });
 });
 
 app.get('*', function (req, res) {
-    res.send("Page Not Found", 404);
+    res.send('Page Not Found', 404);
 });
 
-app.listen(8080);
-console.log("Express Server has started on port 8080");
+db.open(function (err, mongoclient) {
+    if (err) throw err;
+    app.listen(8080);
+    console.log('Express server started on port 8080');
+});
